2f1a8dc6e0129bf1ae02eafd57289ca6c5983421
Alfah <Abdullah.Fahad@brainstation-23.com>
2022-09-02 19:44:40 +0600

    amazon data with xlxs write

--- src/main/java/org/example/amazon.java
+++ src/main/java/org/example/amazon.java
@@ -1,17 +1,27 @@
 package org.example;
 
 import com.microsoft.playwright.*;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class amazon {
-    public static void main(String[] args) throws  InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         try (Playwright playwright = Playwright.create()) {
-            String url = "https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A13896617011&ref=nav_em__nav_desktop_sa_intl_computers_tablets_0_2_6_4";
+
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Productbook");
+
+
+            String url = "https://www.amazon.com/s?i=specialty-aps&bbn=16225007011&rh=n%3A16225007011%2Cn%3A1292115011&ref=nav_em__nav_desktop_sa_intl_monitors_0_2_6_8";
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
             Page page = browser.newPage();
             //Page n[] = new Page[100];
-            for(int i = 1; i<=5; i++) {
+            for(int i = 1; i<=20; i++) {
                 page.navigate(url);
                 String productListXp = "(//span[contains(@class, 'a-size-base-plus')])[" + i + "]";
                 String productNameXp = "//h1[@id= 'title']";
@@ -19,7 +29,7 @@ public class amazon {
                 String productPriceType2Xp = "//span[@class = 'a-price a-text-price a-size-medium apexPriceToPay']";
                 String productShortDescriptionXp = "//ul[@class='a-unordered-list a-vertical a-spacing-mini']";
                 String productLongDescription1Xp = "//div[@id='aplus']";
-                String productLongDescription2Xp = "//div[@class='a-row a-expander-container a-expander-extend-container']//div[@class='a-expander-content a-expander-extend-content']";
+                String productLongDescription2Xp = "//div[@id='prodDetails']";
                 String productLongDescription3Xp = "//div[@id='detailBullets_feature_div']";
                 String imageXp = "//div[@id='imgTagWrapperId']";
                 String img1 = "(//div[@class='ivThumbImage'])[3]";
@@ -34,9 +44,13 @@ public class amazon {
                 String productName = page.innerText(productNameXp);
                 if (page.isVisible(productPriceType1Xp)) {
                     productPrice = page.innerText(productPriceType1Xp);
+                    productPrice = productPrice.substring(1,productPrice.indexOf("\n"));
+                    productPrice = productPrice.replaceAll("[^0-9.]", "");
 //                    System.out.println(productName + " " + productPrice);
                 } else if (page.isVisible(productPriceType2Xp)) {
                     productPrice = page.innerText(productPriceType2Xp);
+                    productPrice = productPrice.substring(1,productPrice.indexOf("\n"));
+                    productPrice = productPrice.replaceAll("[^0-9.]", "");
 //                    System.out.println(productName + " " + productPrice);
                 } else if(page.isVisible(productPriceType1Xp) || page.isVisible(productPriceType2Xp)){
                     int min, max;
@@ -44,11 +58,8 @@ public class amazon {
                     int b = (int)(Math.random()*(max-min+1)+min);
                     productPrice = String.valueOf(b);
                 }
-                productPrice = productPrice.substring(1,productPrice.indexOf("\n"));
-                productPrice = productPrice.replaceAll("[^0-9.]", "");
-                Float price = Float.parseFloat(productPrice);
                 System.out.println(productName);
-                System.out.println(price);
+                System.out.println(productPrice);
                 String currlink = page.url();
                 String SEName = currlink.substring(currlink.indexOf("com/")+4, currlink.indexOf("/dp"));
                 System.out.println(SEName);
@@ -59,12 +70,23 @@ public class amazon {
                 page.waitForLoadState();
                 if (page.isVisible(productLongDescription1Xp)) {
                     productLongDescription = page.innerHTML(productLongDescription1Xp);
+                    System.out.println("des1");
+                    System.out.println(productLongDescription.length());
+                    if (productLongDescription.length() > 30000 && page.isVisible(productLongDescription2Xp)){
+                        productLongDescription = page.innerHTML(productLongDescription2Xp);
+                        System.out.println("des2");
+                    } else if (productLongDescription.length() > 30000 && page.isVisible(productLongDescription3Xp)) {
+                        productLongDescription = page.innerHTML(productLongDescription3Xp);
+                        System.out.println("des3");
+                    }
                 } else if (page.isVisible(productLongDescription2Xp)) {
                     productLongDescription = page.innerHTML(productLongDescription2Xp);
+                    System.out.println("des2out");
                 } else if (page.isVisible(productLongDescription3Xp)) {
                     productLongDescription = page.innerHTML(productLongDescription3Xp);
+                    System.out.println("des3out");
                 }
-                System.out.println(productLongDescription);
+//                System.out.println(productLongDescription);
 
                 page.click(imageXp);
                 page.waitForLoadState();
@@ -81,8 +103,36 @@ public class amazon {
                 System.out.println(img2Url);
                 System.out.println(img3Url);
 
+                Object[][] pro = {
+                        { "Simple Product",0, "TRUE" ,productName, productShortDescription, productLongDescription,"", "Simple product", "FALSE", 0,"","","",SEName, "TRUE", "TRUE", SKU, productPrice, img1Url, img2Url, img3Url}
+                };
+                int rowNum = i;
+                for (Object[] proRow : pro) {
+                    Row row = sheet.createRow(rowNum++);
+                    int colNum = 0;
+                    for (Object field : proRow) {
+                        Cell cell = row.createCell(colNum++);
+                        if (field instanceof String) {
+                            cell.setCellValue((String) field);
+                        } else if (field instanceof Integer) {
+                            cell.setCellValue((Integer) field);
+                        } else if (field instanceof Long) {
+                            cell.setCellValue((Long) field);
+                        } else if (field instanceof Float) {
+                            cell.setCellValue((Float) field);
+                        }
+                    }
+                }
+                try {
+                    FileOutputStream outputStream = new FileOutputStream("Productbook.xlsx");
+                    workbook.write(outputStream);
+                    outputStream.close();
 
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
+
             browser.close();
         }
     }
